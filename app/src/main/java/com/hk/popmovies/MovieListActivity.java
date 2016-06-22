64a900c893ba17c2f4944f4aea80e38e4e109f76
rkheik <rkheik@users.noreply.github.com>
2016-06-22 03:07:52 -0500

    added menu

--- app/src/main/java/com/hk/popmovies/MovieListActivity.java
+++ app/src/main/java/com/hk/popmovies/MovieListActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -23,4 +25,23 @@ public class MovieListActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_movie_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_popular:
+                Log.d("something", "pop selected");
+                return true;
+            case R.id.menu_item_rating:
+                Log.d("something", "rating selected");
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
